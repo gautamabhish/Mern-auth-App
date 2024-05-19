46edd06c4c5081ff79f35256b5c859d7a67d89a5
abhishek <abhishekgautam.080470@gmail.com>
2024-05-19 15:02:31 +0530

    adding routes  using react-router-dom

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from "./Components/Home.jsx"
+import About from "./Components/About.jsx"
+import Profile from "./Components/Profile.jsx"
+import SignIn from "./Components/SignIn.jsx"
+import SignUp from "./Components/SignUp.jsx"
 
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/profile' element={<Profile />}/>
+        <Route path='/sign-in' element={<SignIn />}/>
+        <Route path='/sign-up' element={<SignUp />}/>
+      </Routes>
+    </BrowserRouter>
+        )
 }
-
 export default App
