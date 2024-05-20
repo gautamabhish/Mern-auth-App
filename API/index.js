a0be07e4af36cf9895600777ca2706fca98c14d4
abhishek <abhishekgautam.080470@gmail.com>
2024-05-20 16:17:40 +0530

    Create Test Route

--- API/index.js
+++ API/index.js
@@ -1,11 +1,19 @@
 import express from 'express'
 import mongoose from 'mongoose'
-const app = express()
+import router from './routes/userRoute.js';
+
+const app = express();
+
 mongoose.connect("mongodb://localhost:27017").then(() => {
     console.log("connected")
 }).catch((err) => {
     console.log(err)
-})
+});
+
 app.listen(3000, () => {
-    console.log("server initialised !")
-})
+    console.log("server initialised !");
+});
+
+
+
+app.use("/",router);
---
+++ API/routes/userRoute.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import {testRoute} from '../controllers/userController.js';
+
+const router = express.Router();
+
+
+router.get('/', testRoute);
+
+export default router ;
+
+
+
