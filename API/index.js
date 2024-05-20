1db7baad24faa7a3801a4454c7a67b018088eab3
abhishek <abhishekgautam.080470@gmail.com>
2024-05-20 13:39:47 +0530

    adding local database

--- API/index.js
+++ API/index.js
@@ -1,5 +1,11 @@
 import express from 'express'
+import mongoose from 'mongoose'
 const app = express()
-app.listen(3000,()=>{
+mongoose.connect("mongodb://localhost:27017").then(() => {
+    console.log("connected")
+}).catch((err) => {
+    console.log(err)
+})
+app.listen(3000, () => {
     console.log("server initialised !")
 })
