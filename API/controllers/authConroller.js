a764b5a5653a5cf2795412df5520c162e4ae1eee
abhishek <abhishekgautam.080470@gmail.com>
2024-05-21 19:10:16 +0530

    sign in api

--- API/controllers/authConroller.js
+++ API/controllers/authConroller.js
@@ -1,8 +1,9 @@
 import User from "../models/userModel.js"
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../Utils/error.js"
+import jwt from 'jsonwebtoken'
 
-export const auth = (req, res,next) => {
+export const signup = (req, res,next) => {
     const { userName, email, password } = req.body
     const hashedPassword = bcryptjs.hashSync(password,10)
     const newUser = new User({ userName, email, password : hashedPassword})
@@ -20,6 +21,27 @@ export const auth = (req, res,next) => {
 
     
 
+}
+export const signin = async (req,res,next)=>{
+    const {email , password} = req.body
+    try{
+        const validUser = await User.findOne({email})
+        if(!validUser) {
+            return next(errorHandler(404,'User not found'))
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password)
+        if(!validPassword){
+            return next(errorHandler(401,'wrong credentials'))
+
+        }
+        const {password:hashedPasword,_id :id,...rest} = validUser._doc 
+        const token = jwt.sign({id : validUser._id},'SECRET KEY')
+        const expiryDate  = new Date(Date.now()+3600000)
+        res.cookie('access_token',token,{httpOnly : true ,expires : expiryDate}).status(200).json(rest)
+    }
+    catch(err){
+        next(err)
+    }
 }
 
 
--- API/routes/authRoute.js
+++ API/routes/authRoute.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { auth } from '../controllers/authConroller.js';
+import { signup ,signin} from '../controllers/authConroller.js';
 const router = express.Router();
 
-router.post("/sign-up",auth)
+router.post("/sign-up",signup)
+router.post("/sign-in",signin)
 
 export default router
