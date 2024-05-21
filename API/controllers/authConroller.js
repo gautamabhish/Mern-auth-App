42e5f6f09ce1ade2cae96c18cd8d5e2cead40417
abhishek <abhishekgautam.080470@gmail.com>
2024-05-21 14:07:12 +0530

    add a midleware and a function to handle middleware

--- API/controllers/authConroller.js
+++ API/controllers/authConroller.js
@@ -1,7 +1,8 @@
 import User from "../models/userModel.js"
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../Utils/error.js"
 
-export const auth = (req, res) => {
+export const auth = (req, res,next) => {
     const { userName, email, password } = req.body
     const hashedPassword = bcryptjs.hashSync(password,10)
     const newUser = new User({ userName, email, password : hashedPassword})
@@ -11,7 +12,7 @@ export const auth = (req, res) => {
         res.status(201).json({message: "User saved !"})
         }
         catch(err){
-            res.status(500).json({message : "Encountering Error"})
+            next(err)
   
         }
      }
--- API/index.js
+++ API/index.js
@@ -21,3 +21,13 @@ app.use("/auth/user",router);
 
 app.use("/api/auth",authRoute);
 
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    return res.status(statusCode).json({
+        success : false ,
+        message ,
+        statusCode,
+    });
+});
+
