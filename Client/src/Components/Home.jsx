b5dbf8022e9c635830fb67c565ea68e2f9e70c00
abhishek <abhishekgautam.080470@gmail.com>
2024-05-29 13:56:20 +0530

    completing home page and about page

--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='px-4 py-12 max-w-2xl mx-auto '>
+      <h1 className='text-2xl font-bold mb-4 text-slate-700'>Welcome to MERN Authentication app</h1>
+
+
+      <p className=' mb-4 text-slate-800'> This is a full-stack web application built with the MERN (MongoDB,
+        Express, React, Node.js) stack. It includes authentication features that
+        allow users to sign up, log in, and log out, and provides access to
+        protected routes only for authenticated users.</p>
+
+        <p className=' mb-4 text-slate-800'> The front-end of the application is built with React and uses React
+        Router for client-side routing. The back-end is built with Node.js and
+        Express, and uses MongoDB as the database. Authentication is implemented
+        using JSON Web Tokens (JWT).</p>
+
+        <p> This application is intended as a starting point for building full-stack
+        web applications with authentication using the MERN stack</p>
     </div>
   )
 }
